b5a29c9c9b81b6a194237f69ca8a8b59c38873e4
Johnny Proano <jproano22@yahoo.com>
2024-09-19 11:49:39 -0400

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Johnny Proano on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
